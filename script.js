ba6c64340cdc33d73eed6ba107bda413b42a89b2
Elijah-Igbokah <igbokah@gmail.com>
2022-09-13 11:20:56 +0100

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let elements = document.getElementsByClassName("col");
+let colTitle = document.getElementsByClassName("activeIcon");
 for(var i=0; i < elements.length; i++){
     elements[i].onclick = function(){
         document.querySelectorAll(".col").forEach(el =>{
             el.classList.remove("col-active");
         })
+        document.querySelectorAll(".activeIcon").forEach(el =>{
+            el.classList.remove("activeIcon");
+        })
         this.classList.add("col-active");
+        document.querySelector(".activeIcon").classList.add("activeIcon")
     };
 }
+
+
+
 

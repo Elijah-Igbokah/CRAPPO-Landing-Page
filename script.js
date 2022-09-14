acb1a89b29750d30ec0b5172808c86aefa8415ba
Elijah-Igbokah <igbokah@gmail.com>
2022-09-14 17:15:50 +0100

    fourth commit

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function reveal(){
     for(var i = 0; i < reveals.length; i++){
         let windowHeight = window.innerHeight;
         let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 150
+        let revealPoint = 50
 
         if(revealTop < windowHeight - revealPoint){
             reveals[i].classList.add('activeWindow')
